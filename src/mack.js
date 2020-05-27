0e6f35315323588d1e815da26b5578292c3408ee
mackmcquen <mackmcquen@comcast.net>
2020-05-27 16:55:31 -0500

    basic word display added, still working on getting it to be hidden and the logic behind showing the words.

--- src/mack.js
+++ src/mack.js
@@ -1,10 +1,14 @@
 function main() {
     letterButtons()
+    displayWord()
 }
 
 // Alphabet array for letter select
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+// Words sample array
+let words = ['cat', 'noodle', 'spaceship']
 
 // Iterate over alphabet array and create a button for each letter
 function letterButtons() {
@@ -15,6 +19,13 @@ function letterButtons() {
         `<button type="button" id="letter-button">${alphabet[i].toUpperCase()}</button>`
     }
     console.log(alphabet)
+}
+
+function displayWord() {
+    const wordDisplay = document.querySelector('.word')
+    let randomWord = words[Math.floor(Math.random() * words.length)]
+    let hiddenWord = randomWord.replace(/_/g, "_")
+    wordDisplay.innerHTML += `${hiddenWord}`
 }
 
 main()
